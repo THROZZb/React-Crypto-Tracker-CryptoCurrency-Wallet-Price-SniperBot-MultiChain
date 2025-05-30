dff72a8ab4ff27a01eb33668a656efe59aeb83eb
goworldte <150192570+goworldte@users.noreply.github.com>
2025-05-30 20:41:09 +0300

    Upload Files

---
+++ CryptoRate/CryptoExchanges/BybitExchange.cs
@@ -0,0 +1,44 @@
+﻿using Bitget.Net.Clients;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CryptoRate.CryptoExchanges
+{
+    internal class BybitExchange : ICryptoConnect
+    {
+        public event Action<decimal> RateUpdated;
+        private BitgetSocketClient client;
+        public void Connect()
+        {
+           client = new BitgetSocketClient();
+        }
+
+        public async Task GetRate()
+        {
+            try
+            {
+                decimal lastPrice;
+                Connect();
+
+                var subResult = await client.SpotApi.SubscribeToTickerUpdatesAsync("BTCUSDT", async (update) =>
+                {
+                    lastPrice = Convert.ToDecimal(update.Data.LastPrice);
+                    RateUpdated?.Invoke(lastPrice);
+                });
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Data.ToString());
+            }
+        }
+
+        public async Task UnsubscribeGetRate()
+        {
+            await client.SpotApi.UnsubscribeAllAsync();
+        }
+    }
+}
